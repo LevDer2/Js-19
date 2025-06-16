909b2764f66ed3fb3cdba9a174af65139ebb250c
LevDer2 <123574272+LevDer2@users.noreply.github.com>
2025-06-16 18:40:35 +0300

    Task2

---
+++ js.js/js.js
@@ -0,0 +1,36 @@
+const radioButtons = document.querySelectorAll('input[name="color"]');
+
+radioButtons.forEach(radio => {
+  radio.addEventListener('change', event => {
+    document.body.style.backgroundColor = event.target.value;
+  });
+});
+
+// Task2
+const inputHref = document.getElementById("name-input");
+const pId = document.getElementById("name-output");
+
+inputHref.addEventListener("input", (event) => {
+  if (event.currentTarget.value === "") {
+     console.log(event.currentTarget.value);
+  pId.textContent = `Незнайомець`
+  }
+  else{
+    console.log(event.currentTarget.value);
+  pId.textContent = `${event.currentTarget.value}`
+  }
+})
+
+const idInput = document.getElementById("validation-input");
+idInput.addEventListener('input', () => {
+  const inputLength = idInput.value.length;
+  const requiredLength = Number(idInput.dataset.length);
+
+  if (inputLength >= requiredLength) {
+    idInput.style.color = "green";
+  } else {
+    idInput.style.color = "red";
+  }
+});
+
+// Task 3
